735fd684593c89b50c9c2a231fbb6fc0538af298
xwmdream <xwmdream@qq.com>
2022-05-18 22:15:33 +0800

    feature: 更新api层返回值命名

--- treasure-api/src/main/java/com/github/howwrite/treasure/api/response/AbstractDTO.java
+++ treasure-api/src/main/java/com/github/howwrite/treasure/api/response/AbstractDTO.java
@@ -6,6 +6,6 @@ import java.io.Serializable;
  * @author howwrite
  * @date 2020/10/7 12:20 上午
  */
-public class AbstractInfo implements Serializable {
+public class AbstractDTO implements Serializable {
     private static final long serialVersionUID = 784964219228539790L;
 }
