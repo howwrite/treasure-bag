d80582eb89e285c5b142dd1e95fb2fd93e646060
xwmdream <xwmdream@qq.com>
2022-05-26 23:45:01 +0800

    feature

--- treasure-domain/src/main/java/com/github/howwrite/treasure/domain/model/AbstractModel.java
+++ treasure-domain/src/main/java/com/github/howwrite/treasure/domain/model/AbstractModel.java
@@ -4,7 +4,13 @@ import com.github.howwrite.treasure.domain.value.AbstractValueObject;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
-public abstract class AbstractModel extends AbstractValueObject {
+public abstract class AbstractModel<ID extends Serializable> extends AbstractValueObject {
+    @Serial
+    private static final long serialVersionUID = 2714074658002977618L;
+    private ID id;
 }
--- treasure-domain/src/main/java/com/github/howwrite/treasure/domain/value/AbstractValueObject.java
+++ treasure-domain/src/main/java/com/github/howwrite/treasure/domain/value/AbstractValueObject.java
@@ -2,8 +2,11 @@ package com.github.howwrite.treasure.domain.value;
 
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
 public abstract class AbstractValueObject implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 4215771799080523976L;
 }
