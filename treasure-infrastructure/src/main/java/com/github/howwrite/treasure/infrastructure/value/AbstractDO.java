67574eedc8c8fb97f0c3188c52362a0b74063ee5
howwrite <howwrite@qq.com>
2022-10-09 22:08:21 +0800

    fix: baseDO日期修改为时间类型
    Signed-off-by: howwrite <howwrite@qq.com>

--- treasure-infrastructure/src/main/java/com/github/howwrite/treasure/infrastructure/value/AbstractDO.java
+++ treasure-infrastructure/src/main/java/com/github/howwrite/treasure/infrastructure/value/AbstractDO.java
@@ -4,34 +4,35 @@ import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Data
-public abstract class AbstractDO<ID> implements Serializable {
+public abstract class AbstractDO implements Serializable {
     @Serial
     private static final long serialVersionUID = -2301881368401755059L;
 
     /**
      * id
      */
-    private ID            id;
+    private Long id;
     /**
      * 是否删除
      * 0为未删除，否则表示删除
      */
-    private Long          deleted;
+    private Long deleted;
     /**
      * 删除时间
      */
-    private Long deletedTime;
+    private LocalDateTime deletedTime;
 
     /**
      * 记录创建时间
      */
-    private Long createdTime;
+    private LocalDateTime createdTime;
 
     /**
      * 记录更新时间
      */
-    private Long updatedTime;
+    private LocalDateTime updatedTime;
 
 }
