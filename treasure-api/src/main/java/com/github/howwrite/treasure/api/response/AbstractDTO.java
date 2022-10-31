9a7963b82812c1a7e9236a6b1a3ad0e912ba0269
howwrite <howwrite@qq.com>
2022-10-31 23:16:43 +0800

    feature: 增加注释
    Signed-off-by: howwrite <howwrite@qq.com>

--- treasure-api/src/main/java/com/github/howwrite/treasure/api/response/AbstractDTO.java
+++ treasure-api/src/main/java/com/github/howwrite/treasure/api/response/AbstractDTO.java
@@ -4,6 +4,9 @@ import java.io.Serial;
 import java.io.Serializable;
 
 /**
+ * 不希望子类直接透出id，但是我不知道怎么可以在父类把子类的id属性屏蔽了。。。
+ * 那还是看看这行注释别透出id了。不为啥，就是不想透出。
+ *
  * @author howwrite
  * @date 2020/10/7 12:20 上午
  */
