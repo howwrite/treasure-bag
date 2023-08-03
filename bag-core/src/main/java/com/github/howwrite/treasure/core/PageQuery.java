cd1e3973bd091120bca908e9cf3d35c358bf7cee
howwrite <howwrite@qq.com>
2023-08-03 21:35:01 +0800

    add field
    Signed-off-by: howwrite <howwrite@qq.com>

--- bag-core/src/main/java/com/github/howwrite/treasure/core/PageQuery.java
+++ bag-core/src/main/java/com/github/howwrite/treasure/core/PageQuery.java
@@ -2,6 +2,9 @@ package com.github.howwrite.treasure.core;
 
 public interface PageQuery {
 
+    Integer DEFAULT_PAGE_NO = 1;
+    Integer DEFAULT_PAGE_SIZE = 10;
+
 
     default Integer getOffset() {
         Integer no = getPageNo();
