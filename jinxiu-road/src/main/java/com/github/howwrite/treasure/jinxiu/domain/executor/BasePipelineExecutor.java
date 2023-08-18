1e00f608e8cdd787d2faf98de84e3ecb7b130a7e
howwrite <howwrite@qq.com>
2023-08-18 21:58:35 +0800

    feat: 增加执行方法名称
    Signed-off-by: howwrite <howwrite@qq.com>

--- jinxiu-road/src/main/java/com/github/howwrite/treasure/jinxiu/domain/executor/BasePipelineExecutor.java
+++ jinxiu-road/src/main/java/com/github/howwrite/treasure/jinxiu/domain/executor/BasePipelineExecutor.java
@@ -3,13 +3,18 @@ package com.github.howwrite.treasure.jinxiu.domain.executor;
 import com.github.howwrite.treasure.core.ServerBizException;
 import com.github.howwrite.treasure.jinxiu.domain.context.Context;
 import com.github.howwrite.treasure.jinxiu.domain.node.Node;
+import com.google.common.collect.Sets;
 
 import javax.annotation.Nonnull;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Set;
 
 public abstract class BasePipelineExecutor implements PipelineExecutor {
+
+    private final static Set<String> EXECUTE_METHOD_NAMES = Sets.newHashSet("execute", "e");
+
     public final void executeRun(@Nonnull Node node, @Nonnull Context context) {
         try {
             Method executeMethod = findExecuteMethod(node.getClass());
@@ -26,7 +31,7 @@ public abstract class BasePipelineExecutor implements PipelineExecutor {
     @Nonnull
     public Method findExecuteMethod(@Nonnull Class<?> clazz) {
         for (Method declaredMethod : clazz.getDeclaredMethods()) {
-            if ("execute".equals(declaredMethod.getName())) {
+            if (EXECUTE_METHOD_NAMES.contains(declaredMethod.getName())) {
                 return declaredMethod;
             }
         }
