152be60fb8ddbc81432e044d6e7588c77b5a1bb8
howwrite <howwrite@qq.com>
2023-08-29 23:04:54 +0800

    feat: 增加api
    Signed-off-by: howwrite <howwrite@qq.com>

--- bag-core/src/main/java/com/github/howwrite/treasure/core/utils/DateUtil.java
+++ bag-core/src/main/java/com/github/howwrite/treasure/core/utils/DateUtil.java
@@ -1,6 +1,6 @@
 package com.github.howwrite.treasure.core.utils;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -10,13 +10,22 @@ public class DateUtil {
     private static final Map<String, DateTimeFormatter> FORMATTER_MAP = new ConcurrentHashMap<>();
 
 
-    public static LocalDateTime strToLocalDateTime(String dateString, String formatStr) {
+    public static LocalDate str2LocalDate(String dateStr, String formatStr) {
         DateTimeFormatter dateTimeFormatter = FORMATTER_MAP.get(formatStr);
         if (dateTimeFormatter == null) {
             dateTimeFormatter = DateTimeFormatter.ofPattern(formatStr);
             FORMATTER_MAP.put(formatStr, dateTimeFormatter);
         }
-        return LocalDateTime.parse(dateString, dateTimeFormatter);
+        return LocalDate.parse(dateStr, dateTimeFormatter);
+    }
+
+    public static String localDate2Str(LocalDate localDate, String formatStr) {
+        DateTimeFormatter dateTimeFormatter = FORMATTER_MAP.get(formatStr);
+        if (dateTimeFormatter == null) {
+            dateTimeFormatter = DateTimeFormatter.ofPattern(formatStr);
+            FORMATTER_MAP.put(formatStr, dateTimeFormatter);
+        }
+        return localDate.format(dateTimeFormatter);
     }
 
 }
