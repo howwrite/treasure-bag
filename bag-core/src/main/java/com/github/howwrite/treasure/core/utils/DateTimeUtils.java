75eb7902c143909a6a8431836b0fea6c6439ca69
howwrite <howwrite@qq.com>
2025-03-29 22:54:24 +0800

    DateTimeUtils.java
    Signed-off-by: howwrite <howwrite@qq.com>

--- bag-core/src/main/java/com/github/howwrite/treasure/core/utils/DateTimeUtils.java
+++ bag-core/src/main/java/com/github/howwrite/treasure/core/utils/DateTimeUtils.java
@@ -1,14 +1,33 @@
 package com.github.howwrite.treasure.core.utils;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class DateUtils {
+public class DateTimeUtils {
 
     private static final Map<String, DateTimeFormatter> FORMATTER_MAP = new ConcurrentHashMap<>();
 
+
+    public static LocalDateTime str2LocalDateTime(String dateTimeStr, String formatStr) {
+        DateTimeFormatter dateTimeFormatter = FORMATTER_MAP.get(formatStr);
+        if (dateTimeFormatter == null) {
+            dateTimeFormatter = DateTimeFormatter.ofPattern(formatStr);
+            FORMATTER_MAP.put(formatStr, dateTimeFormatter);
+        }
+        return LocalDateTime.parse(dateTimeStr, dateTimeFormatter);
+    }
+
+    public static String localDateTime2Str(LocalDateTime localDate, String formatStr) {
+        DateTimeFormatter dateTimeFormatter = FORMATTER_MAP.get(formatStr);
+        if (dateTimeFormatter == null) {
+            dateTimeFormatter = DateTimeFormatter.ofPattern(formatStr);
+            FORMATTER_MAP.put(formatStr, dateTimeFormatter);
+        }
+        return localDate.format(dateTimeFormatter);
+    }
 
     public static LocalDate str2LocalDate(String dateStr, String formatStr) {
         DateTimeFormatter dateTimeFormatter = FORMATTER_MAP.get(formatStr);
