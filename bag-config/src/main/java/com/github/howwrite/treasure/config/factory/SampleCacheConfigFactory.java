d9d427c35d80b6dfebf796942bcb9249606a6fbc
howwrite <xwmdream@qq.com>
2025-05-25 22:50:51 +0800

    cache null value

--- bag-config/src/main/java/com/github/howwrite/treasure/config/factory/SampleCacheConfigFactory.java
+++ bag-config/src/main/java/com/github/howwrite/treasure/config/factory/SampleCacheConfigFactory.java
@@ -7,24 +7,25 @@ import com.google.common.cache.LoadingCache;
 import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nonnull;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 public class SampleCacheConfigFactory implements ConfigFactory {
 
-    LoadingCache<Pair<String, String>, String> kkvCache = CacheBuilder.newBuilder()
+    LoadingCache<Pair<String, String>, Optional<String>> kkvCache = CacheBuilder.newBuilder()
             .maximumSize(1000)
             .expireAfterAccess(10, TimeUnit.MINUTES)
             .build(new CacheLoader<>() {
                 @Nonnull
                 @Override
-                public String load(@Nonnull Pair<String, String> key) {
-                    return ConfigFetcherProvider.provideConfigFetcher(key.getKey(), key.getValue()).readConfig(key.getKey(), key.getValue());
+                public Optional<String> load(@Nonnull Pair<String, String> key) {
+                    return Optional.ofNullable(ConfigFetcherProvider.provideConfigFetcher(key.getKey(), key.getValue()).readConfig(key.getKey(), key.getValue()));
                 }
             });
 
     @Override
     public String readConfig(String namespace, String key) {
-        return kkvCache.getUnchecked(Pair.of(namespace, key));
+        return kkvCache.getUnchecked(Pair.of(namespace, key)).orElse(null);
     }
 
     @Override
